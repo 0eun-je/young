e4dafe1f706dd36f5a00b880ed84c749c13cc125
0eun-je <sangria16@naver.com>
2021-08-19 02:45:53 +0900

    0245

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -83,7 +83,7 @@ const PhotoBlock = styled.div`
     .photo {
         width: 380px;
         height: 380px;
-        background: url("../young/public/profile_photo.jpg") no-repeat;
+        background: url("../young/static/media/profile_photo.jpg") no-repeat;
         background-size: 100% 100%;
         border-radius: 50%;
     }
